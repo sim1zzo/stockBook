ea6433608befb7e902d772f8b2163f2e6e64dcc7
Simone Izzo <itausa6@gmail.com>
2020-09-28 00:39:38 +0100

    section 4.1 added product implementation but upsert

--- StockBook.DataAccess/Data/ApplicationDbContext.cs
+++ StockBook.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace StockBook.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ StockBook.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using StockBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockBook.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
