ea73ba43620ec55c5cfe9c55742dac2a1c0eba8c
Simone Izzo <itausa6@gmail.com>
2020-09-29 18:04:40 +0100

    section 5.5 added email implementation.

--- StockBook/Startup.cs
+++ StockBook/Startup.cs
@@ -37,6 +37,7 @@ namespace StockBook
             services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<EmailOptions>(Configuration);
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
